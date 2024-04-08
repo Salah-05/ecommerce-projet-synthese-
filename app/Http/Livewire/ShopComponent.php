574e8061e9c68524105a058c3fd1e5722de9a0fe
achraf <achrafboujattou72@gmail.com>
2024-04-08 14:35:36 +0000

    category by name

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Livewire;
 
-
+use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -45,7 +45,7 @@ class ShopComponent extends Component
         else{
             $products =  Product::paginate($this->pageSize);
         }
-
-        return view('livewire.shop-component',['products'=>$products]);
+        $categories = Category::orderBy('name','ASC')->get();
+        return view('livewire.shop-component',['products'=>$products , 'categories'=>$categories]);
     }
 }
