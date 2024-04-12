e54ab7525f887c13ae85000fcc3b3477bf975ebc
Salah eddine <salaheddine.elhabouni@gmail.com>
2024-04-12 15:51:21 +0000

    creer une  fonction de tri (change page size)

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -9,6 +9,7 @@ use Cart;
 class ShopComponent extends Component
 {
     use WithPagination;
+    public $pageSize = 12;
     public function store($product_id,$product_name,$product_price)
     {
         Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
@@ -17,10 +18,13 @@ class ShopComponent extends Component
         return back(); //rediriger l'utilisateur vers la même page sur laquelle il se trouve actuellement
 
     }
+    public function changePageSize($size){
+        $this->pageSize = $size;
+    }
 
     public function render()
     {
-        $products= Product::paginate(9);
+        $products= Product::paginate($this->pageSize);
         return view('livewire.shop-component',['products'=>$products]);
     }
 }
