036a5f36cfe5ca4e5f759de37947e0a937786888
Salah eddine <salaheddine.elhabouni@gmail.com>
2024-04-12 15:03:59 +0000

    l'icon de panier devient dynamique

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -11,22 +11,27 @@ class CartComponent extends Component
         $product = Cart::get($rowId);
         $qty = $product->qty + 1;
         Cart::update($rowId,$qty);
+        $this->emitTo('cart-icon-component','refreshComponent');
        }
 
        public function decreasequantity($rowId){
         $product = Cart::get($rowId);
         $qty = $product->qty - 1;
         Cart::update($rowId,$qty);
+        $this->emitTo('cart-icon-component','refreshComponent');
        }
 
        public function destroy($id)
        {
         Cart::remove($id);
+        $this->emitTo('cart-icon-component','refreshComponent');
         session()->flash('success_message','Item has been removed');
+       
        }
        
        public function clearAll(){
         Cart::destroy();
+        $this->emitTo('cart-icon-component','refreshComponent');
        }
        
     public function render()
