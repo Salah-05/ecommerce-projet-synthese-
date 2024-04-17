753c78a9b1a032ca246b3ae00b4efafa5004dc63
achraf <achrafboujattou72@gmail.com>
2024-04-17 09:33:29 +0100

    admin Category

---
+++ app/Http/Livewire/Admin/AdminAddCategoriesComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminAddCategoriesComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-add-categories-component');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
@@ -49,6 +50,7 @@ Route::middleware(('auth'))->group(function(){
 
 Route::middleware(['auth','authadmin'])->group(function(){
     Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/categories',AdminCategoriesComponent::class)->name('admin.categories');
 });
 
 require __DIR__.'/auth.php';
